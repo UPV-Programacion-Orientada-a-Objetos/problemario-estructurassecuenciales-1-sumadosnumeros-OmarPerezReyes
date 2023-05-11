5dad70bf14b8eb3e7f4d36b5fc7dfd9a88219268
OmarPerezReyes <perezreyesomaralejandro@gmail.com>
2023-05-11 14:37:15 -0600

    Entrega 1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
-
     public static void main(String[] args) {
-
+        SumaDosNumeros sumaDosNumeros = new SumaDosNumeros();
+		System.out.println(sumaDosNumeros.suma(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
     }
 }
